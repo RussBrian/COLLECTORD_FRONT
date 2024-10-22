4bf4f3012d5d17be7250d0dd8d942146b56debc8
Daveyi <daveyigonzalez@gmail.com>
2024-10-21 22:30:19 -0400

    Estructura de ForgotPassword

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,18 +1,60 @@
-import { Link } from "../components/Link";
+import { useForm } from "react-hook-form";
+import { InputField } from "../components/ui/InputField.jsx";
+import { Button } from "../components/ui/Button.jsx";
+import { Link } from "../components/Link.jsx";
+import { HiArrowSmallLeft } from "react-icons/hi2";
+import '../styles/registerForm.css';
+import '../styles/registerUser.css';
 
+const ForgotPassword = () => {
+    const { control, handleSubmit } = useForm();
 
-const ForgotPasswordPage = () => {
-    return(
-        <div className="container">
-            <h1>La macaste manito, no vas a poder entrar</h1>
+    const handleFormSubmit = (data) => {
+        console.log("Datos del formulario:", data);
+        // Aquí iría la lógica para enviar el correo de restablecimiento de contraseña
+    };
 
-            <div>
-                <Link to='/login'>
-                <button>Vuelve al Login</button>
-                </Link>
+    return (
+        <div className='main-container'>
+            <div className='form-container'>
+                <h3 className='text-gray-500 font-bold'>Olvidé mi contraseña</h3>
+
+                <form onSubmit={handleSubmit(handleFormSubmit)}>
+                    <label className='custom-label'>Correo electrónico</label>
+                    <InputField 
+                        control={control} 
+                        name="email" 
+                        type="email" 
+                        style={'custom-input'} 
+                        rules={{ 
+                            required: 'El correo es requerido', 
+                            pattern: { 
+                                value: /^\S+@\S+\.\S+$/, 
+                                message: 'Correo no válido' 
+                            }
+                        }}
+                    />
+                    
+                    <div>
+                        <p className='custom-message-required'>Ingresa el correo para recuperar tu contraseña.</p>
+                    </div>
+
+                    <div className='buttons-container'>
+                        <Button className='custom-next-Btn custom-next-Btn-apparience' text="Enviar" />
+                        
+                        <Link to='/login'>
+                                <div className='return-login-btn'>
+                                    <button className='return-login-btn-apparience' onClick={() => setStep(1)}>
+                                        Volver al inicio
+                                    </button>
+                                </div>
+                            </Link>
+                    </div>
+                </form>
             </div>
+            <aside className="aside-background"></aside>
         </div>
-    )
-}
+    );
+};
 
-export default ForgotPasswordPage;
+export default ForgotPassword;
